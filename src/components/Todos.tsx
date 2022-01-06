5e167d6e7f7fd69952c7825864175f06289df0f5
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2022-01-06 11:33:24 +0200

    Add the delete functionality

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -7,11 +7,13 @@ import classes from './Todos.module.css';
 // Specifies the children attribute from props (already-built in)
 
 // Programmer describes the passing props between: <{...}>
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[], onRemoveTodo: (id: string) => void }> = (props) => {
     return (
         <ul className={classes.todos}>
             {props.items.map(item => (
-                <TodoItem key={item.id} text={ item.text}/>
+                // .bind is used to pre configure a function for future execution.
+                //
+                <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id) }/>
             ))}
         </ul>
     );
